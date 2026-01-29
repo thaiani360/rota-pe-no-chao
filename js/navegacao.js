938db6be4314562c77f72471cc45a642f9a6114d
thaiani360 <thaianibarbosasouza13@gmail.com>
2026-01-29 15:04:49 -0300

    Add files via upload

---
+++ js/navegacao.js
@@ -0,0 +1,51 @@
+window.addEventListener("load", () => {
+
+  const ordemBase = [
+    "amorosa.html",
+    "pedra-sino.html",
+    "escorrega.html",
+    "pico.html",
+    "indiana.html"
+  ];
+
+  const paginaAtual = location.pathname.split("/").pop();
+  const btnProximo = document.querySelector(".btn-proximo");
+
+  if (!btnProximo) return;
+
+  if (paginaAtual === "index.html" || paginaAtual === "") {
+    sessionStorage.removeItem("rotaOrdem");
+    sessionStorage.removeItem("rotaInicio");
+    return;
+  }
+
+  if (!ordemBase.includes(paginaAtual)) {
+    btnProximo.style.display = "none";
+    return;
+  }
+
+  if (!sessionStorage.getItem("rotaInicio")) {
+    sessionStorage.setItem("rotaInicio", paginaAtual);
+
+    const indexInicio = ordemBase.indexOf(paginaAtual);
+
+    const rotaOrdem = [
+      ...ordemBase.slice(indexInicio),
+      ...ordemBase.slice(0, indexInicio)
+    ];
+
+    sessionStorage.setItem("rotaOrdem", JSON.stringify(rotaOrdem));
+  }
+
+  const rotaOrdem = JSON.parse(sessionStorage.getItem("rotaOrdem"));
+  const indexAtual = rotaOrdem.indexOf(paginaAtual);
+
+  if (indexAtual === rotaOrdem.length - 1) {
+    btnProximo.style.display = "none";
+    return;
+  }
+
+  btnProximo.style.display = "inline-flex";
+  btnProximo.href = rotaOrdem[indexAtual + 1];
+
+});
